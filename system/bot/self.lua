9c9d0ef5aa20c87e5ed0f59c7ac151f0522778f1
ABIZ <KOJENI74@YAHOO.COM>
2016-09-11 12:09:00 +0430

    Update self.lua

--- system/bot/self.lua
+++ system/bot/self.lua
@@ -227,7 +227,7 @@ function create_config( )
 	 "version",
 	 "poker"
     },
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {260860824,125034353,tonumber(our_id)},--Sudo users
     moderation = {data = 'system/data/adv-self.json'},
     about_text = [[*IN THE NAME OF ALLAH*
 This is an original bot and based on (AdvanSource).
